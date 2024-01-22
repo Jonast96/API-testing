baf4011626e0edca03a58cfca1596fad95c45432
Jonast96 <Jon.Astveit@hotmail.com>
2024-01-22 19:34:18 +0100

    database connection

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
+const mongoose = require("mongoose");
+
 const dotenv = require("dotenv");
 const app = require("./app");
 
+dotenv.config({
+  path: "./config.env",
+});
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD,
+);
+
+console.log(DB);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB connection successful");
+  });
 dotenv.config({ path: "./config.env" });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+app.listen(port, () => {});
