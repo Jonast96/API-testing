54442ccfed80ee99b78a17f97c1ac201e1ecd365
Jonast96 <Jon.Astveit@hotmail.com>
2024-01-24 16:02:11 +0100

    adding tours to database

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const DB = process.env.DATABASE.replace(
   process.env.DATABASE_PASSWORD,
 );
 
+dotenv.config({ path: "./config.env" });
 console.log(DB);
 
 mongoose
@@ -24,7 +25,39 @@ mongoose
   .then(() => {
     console.log("DB connection successful");
   });
-dotenv.config({ path: "./config.env" });
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"], // validator
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
+
+const testTour = new Tour({
+  name: "The forest hiker",
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log("ERROR: ", err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {});
