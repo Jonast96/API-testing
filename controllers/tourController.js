9b7eb7f8423c29b700e9d3042396a3eb08a0195f
Jonast96 <Jon.Astveit@hotmail.com>
2024-02-22 13:18:11 +0100

    aliasing

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,12 @@
 const Tour = require("../models/tourModel");
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = "5";
+  req.query.sort = "-ratingsAverage,price";
+  req.query.fields = "name,price,ratingsAverage,summary,difficulty";
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     const queryObj = { ...req.query };
@@ -37,6 +44,7 @@ exports.getAllTours = async (req, res) => {
       if (skip >= numTours) throw new Error("This page does not exist");
     }
 
+    // Execute query
     const tours = await query;
 
     res.status(200).json({
