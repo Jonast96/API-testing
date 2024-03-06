3b62b06e3b0cc20e0f23ce198d2727a27df2fca0
Jonast96 <Jon.Astveit@hotmail.com>
2024-03-06 19:02:51 +0100

    unhandled routes

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-// const slugify = require("slugify");
+const slugify = require("slugify");
 
 const tourSchema = new mongoose.Schema(
   {
@@ -70,14 +70,10 @@ tourSchema.virtual("durationWeeks").get(function () {
 });
 
 //DOCUMENT MIDDLEWARE: runs before .save() and .create()
-// tourSchema.pre("save", function (next) {
-//   this.slug = slugify(this.name, { lower: true });
-//   next();
-// });
-
-// tourSchema.pre("find", (next) => {
-//   next();
-// });
+tourSchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
 
 const Tour = mongoose.model("Tour", tourSchema);
 
